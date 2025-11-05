1d3d3f048b18798aedf2cbc6df21a6c28a096d74
Nurul Badriyah <badriyahnurul575@gmail.com>
2025-11-05 14:23:08 +0700

    Initial commit

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php include 'header.php'; ?>
+<div class="container-fluid">
+	<div class="row">
+		<div class="container konten-wrapper">
+			<div class="panel panel-default">
+				<div class="panel-body main">
+					<div class="col-md-8">
+						<div class="about">
+							<div class="row post-title">
+								<div class="col-sm-12">
+									<h2>TENTANG BERITA KITA</h2>
+								</div>
+							</div>
+							<div class="row post-content">
+								<div class="col-sm-12">
+									<p>Berita Kita adalah portal informasi yang menyajikan berita aktual, terpercaya, dan bermanfaat bagi masyarakat Indonesia. Kami berkomitmen untuk memberikan sajian berita yang akurat dan berimbang dalam berbagai kategori seperti Nasional, Olahraga, dan Teknologi.</p>
+										<p>Berita Kita — Sumber informasi terpercaya untuk Anda.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php include 'sidebar.php'; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include 'footer.php';
